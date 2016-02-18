a599aab18168e38dc2573962a1801deaf84c9774
Ben Richards <benjamindrichards@gmail.com>
2016-02-19 10:52:21 +1300

    Fix issue with out-of-order boundaries.
    As seen in `stick.brd`, if the board designer didn't create the board boundary wire definitions in the correct order, they wouldn't render correctly. In fact, you could define boundary wires to start from the same location and diverge! This would never be caught by the basic path sort that I implemented.
    
    I've now set it up to actually convert the coordinates to run in a single direction while sorting the boundary wires, both in `_buildDepthEdges()` and in `renderBounds()`. It shouldn't be entirely necessary in `_buildDepthEdges()`, which is running off the same wire definitions that are modified in `renderBounds()`, but it can't hurt to use the most robust reordering mechanism available.

--- src/eagle-brd-renderer.js
+++ src/eagle-brd-renderer.js
@@ -406,11 +406,31 @@ EagleBrdRenderer.prototype._buildDepthEdges = function( add ) {
 
 		for ( j = 0; j < wireGrp.length; j++ ) {
 			wire = wireGrp[ j ];
-			x = wire.getAttribute( "x2" );
-			y = wire.getAttribute( "y2" );
-			for ( k = j; k >= 0; k-- ) {
-				if ( x === wireGrp[ k ].getAttribute( "x1" ) &&
-						y === wireGrp[ k ].getAttribute( "y1" ) ) {
+			x1 = wire.getAttribute( "x1" );
+			y1 = wire.getAttribute( "y1" );
+			x2 = wire.getAttribute( "x2" );
+			y2 = wire.getAttribute( "y2" );
+			for ( k = j - 1; k >= 0; k-- ) {
+				x = wireGrp[ k ].getAttribute( "x1" );
+				y = wireGrp[ k ].getAttribute( "y1" );
+				if ( x2 === x && y2 === y ) {
+
+					// Wire connects to place K
+					wireGrp.splice( j, 1 );
+					wireGrp.splice( k, 0, wire );
+					break;
+				} else if ( x1 === x && y1 === y ) {
+
+					// Wire inversely connects to place K
+					wire.setAttribute( "x1", x2 );
+					wire.setAttribute( "y1", y2 );
+					wire.setAttribute( "x2", x1 );
+					wire.setAttribute( "y2", y1 );
+					if ( wire.hasAttribute( "curve" ) ) {
+						wire.setAttribute(
+							"curve",
+							"" + -parseFloat( wire.getAttribute( "curve" ) ) );
+					}
 					wireGrp.splice( j, 1 );
 					wireGrp.splice( k, 0, wire );
 					break;
@@ -2187,7 +2207,7 @@ EagleBrdRenderer.prototype.renderBounds = function() {
 	**/
 
 	var chordData, holes, pads,
-		i, j, k, x, y, firstX, firstY, lastX, lastY,
+		i, j, k, x, y, x1, y1, x2, y2, firstX, firstY, lastX, lastY,
 		wireGrps, wireGrp, wire,
 		layer = this.getLayer( "Bounds" ),
 		wires = layer.getElements( "wire" );
@@ -2223,11 +2243,31 @@ EagleBrdRenderer.prototype.renderBounds = function() {
 
 		for ( j = 0; j < wireGrp.length; j++ ) {
 			wire = wireGrp[ j ];
-			x = wire.getAttribute( "x2" );
-			y = wire.getAttribute( "y2" );
-			for ( k = j; k >= 0; k-- ) {
-				if ( x === wireGrp[ k ].getAttribute( "x1" ) &&
-						y === wireGrp[ k ].getAttribute( "y1" ) ) {
+			x1 = wire.getAttribute( "x1" );
+			y1 = wire.getAttribute( "y1" );
+			x2 = wire.getAttribute( "x2" );
+			y2 = wire.getAttribute( "y2" );
+			for ( k = j - 1; k >= 0; k-- ) {
+				x = wireGrp[ k ].getAttribute( "x1" );
+				y = wireGrp[ k ].getAttribute( "y1" );
+				if ( x2 === x && y2 === y ) {
+
+					// Wire connects to place K
+					wireGrp.splice( j, 1 );
+					wireGrp.splice( k, 0, wire );
+					break;
+				} else if ( x1 === x && y1 === y ) {
+
+					// Wire inversely connects to place K
+					wire.setAttribute( "x1", x2 );
+					wire.setAttribute( "y1", y2 );
+					wire.setAttribute( "x2", x1 );
+					wire.setAttribute( "y2", y1 );
+					if ( wire.hasAttribute( "curve" ) ) {
+						wire.setAttribute(
+							"curve",
+							"" + -parseFloat( wire.getAttribute( "curve" ) ) );
+					}
 					wireGrp.splice( j, 1 );
 					wireGrp.splice( k, 0, wire );
 					break;
@@ -2240,7 +2280,6 @@ EagleBrdRenderer.prototype.renderBounds = function() {
 	for ( i = 0; i < wireGrps.length; i++ ) {
 		wires = wires.concat( wireGrps[ i ] );
 	}
-
 
 
 	// Draw wire outlines
